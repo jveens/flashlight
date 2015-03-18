67167c7a06c46830ff7f272b72c50ea2dd321d9b
jveens <jennyveens@gmail.com>
2015-03-18 14:25:36 -0400

    Added Prism

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 $(function() {
-	$('body').flashlight(200, 0.85, true);
+	$('body').flashlight(200, 0.75, true);
 	
 	var shine = new Shine(document.getElementById('shine'));
-	window.addEventListener('mousemove', function(event) {
+	window.addEventListener('mousemo6e', function(event) {
 	  shine.light.position.x = event.clientX;
 	  shine.light.position.y = event.clientY;
 	  shine.draw();
